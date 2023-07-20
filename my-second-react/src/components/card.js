97ed4d00f730362cb10cc3fd99006bc119a6bb6c
sulistyadewi <sulistya2coding@gmail.com>
2023-07-20 20:56:50 +0700

    membuat card

--- my-second-react/src/components/card.js
+++ my-second-react/src/components/card.js
@@ -2,11 +2,7 @@ import React from "react";
 
 const Card = () => {
   <div className="">
-    <img
-      className="p-5"
-      src="https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
-      alt=""
-    />
+    <img className="p-5" src="./public/images/book.jpg" alt="" />
     <div>
       <h2>Sulistya</h2>
       <h4>Student</h4>
